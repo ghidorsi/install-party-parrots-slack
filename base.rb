9d0df92d89e6d948d7ba9ee7410c89ea81116356
ghidorsi <ghidorsi@thoughtworks.com>
2020-08-12 13:03:16 -0300

    Improving performance removing sleeps, adding the newest zip from party parrots, updating libraries, fixing new classes from slack and installing hd parrots

--- base.rb
+++ base.rb
@@ -1,5 +1,6 @@
 require 'selenium-webdriver'
 require 'io/console'
+require 'io/console'
 require 'ruby-progressbar'
 
 app_dir = File.expand_path(File.dirname(__FILE__))
@@ -8,23 +9,23 @@ parrots_dir = app_dir + '/parrots/'
 unless Dir.exists?(parrots_dir)
   puts "First, lets download all the parrots!"
 
-  `wget -q http://cultofthepartyparrot.com/parrots-7ee809e0f0.zip -P #{app_dir}`
-  `unzip -q #{app_dir + '/parrots-7ee809e0f0.zip'} -d #{parrots_dir}`
-  `cp -r #{parrots_dir + '/parrots'} #{app_dir} && rm -rf #{parrots_dir + '/parrots'} #{parrots_dir + '/hd'} `
-  `rm -f #{app_dir + '/parrots-7ee809e0f0.zip'}`
+  `wget -q https://cultofthepartyparrot.com/parrots-fc0dac9ef3.zip -P #{app_dir}`
+  `unzip -q #{app_dir + '/parrots-fc0dac9ef3.zip'} -d #{app_dir}`
+  `cp -r #{parrots_dir + '/hd/.'} #{parrots_dir} && rm -rf #{parrots_dir + '/hd'}`
+  `rm -f #{app_dir + '/parrots-fc0dac9ef3.zip'}`
 
   puts "All set here, doctor!\n\n"
 end
 
 puts "Please, insert your organization slack url:"
 print "https://"
-slack_path = "https://" + gets.chomp
+slack_path = 'https://' + gets.chomp
 
 puts "\nInsert your user email:"
 slack_email = gets.chomp
 
 puts "\nNow insert your super secret password"
-slack_password = STDIN.noecho(&:gets).chomp
+slack_password = IO::console.getpass
 
 puts "\nNow that we have it all, lets do this! :fast_parrot:"
 
@@ -46,33 +47,37 @@ driver.find_element(id: 'email').send_keys(slack_email)
 password_field.send_keys(slack_password)
 password_field.submit
 
-begin
-  abort('\nWrong email/password mate, try again please') if driver.find_element(class: 'alert_error').displayed?
-rescue => NoSuchElementError
-  puts "\nLogin has been successfully made, are you feeling the parrots already??\n\n"
-end
+abort('\nWrong email/password mate, try again please') if driver.find_elements(id: 'password_error').any?
+
+puts "\nLogin has been successfully made, are you feeling the parrots already??\n\n"
 
 parrots_with_directory = Dir.glob(parrots_dir + '*.gif')
 
 progress_bar = ProgressBar.create(title: 'Parroting your slack!', total: parrots_with_directory.length, format: "\e[0;32m%t: |%B|\e[0m")
 
-add_emoji_btn = wait.until {
-    element = driver.find_element(class: "p-customize_emoji_wrapper__custom_button")
-    element if element.displayed?
-  }
+add_emoji_btn_lambda = -> do
+    wait.until {
+      element = driver.find_element(xpath: "//button[@data-qa='customize_emoji_add_button']")
+      element if element.displayed?
+    }
+  end
+
+add_emoji_btn_lambda.call
 
 parrots_with_directory.each do |parrot_with_directory|
+  wait.until { 
+    if driver.find_elements(xpath: "//div[@data-qa='customize_emoji_add_dialog_error']").any?
+      driver.find_element(xpath: "//button[@data-qa='sk_close_modal_button']").click
+    end
+
+    driver.find_elements(class: 'c-sk-modal_portal').none? 
+  }
+
   parrot_name = parrot_with_directory.gsub(parrots_dir, '')
   parrot_id = parrot_name.gsub(/.gif/, '').tr('-', '').split(/(?=parrot)/).join('-')
   progress_bar.title = parrot_name
 
-  # skip if the account already has this emoji
-  if driver.find_element(class: "p-customize_emoji_list__container").text.include?(":"+parrot_id+":")
-    progress_bar.increment
-    next
-  end
-
-  add_emoji_btn.click
+  add_emoji_btn_lambda.call.click
 
   name_el = wait.until {
     element = driver.find_element(id: 'emojiname')
@@ -82,9 +87,7 @@ parrots_with_directory.each do |parrot_with_directory|
   name_el.clear()
   name_el.send_keys(parrot_id)
   driver.find_element(id: 'emojiimg').send_keys(parrots_dir + parrot_name)
-  driver.find_element(class: 'c-dialog__go').click
-
-  sleep 2 # Wait image upload (this could take longer, change at your will)
+  driver.find_element(xpath: "//button[@data-qa='customize_emoji_add_dialog_go']").click
 
   progress_bar.increment
 end
